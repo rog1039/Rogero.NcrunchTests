ca7978eb97d9f1bcf7fc11a0705e500aa6e44ff9
Paul Rogero <progero@gmail.com>
2016-01-14 08:44:49 -0500

    Updated code to be clearer.

---
+++ Rogero.NcrunchTests.SharedState.Tests/Tests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+using Xunit;
+
+namespace Rogero.NcrunchTests.SharedState.Tests
+{
+    public class ContextSharedStateTests
+    {
+        private DatabaseContext _context;
+        private int _onModelCreatingCallCount = 0;
+        private Action _action;
+        private DbContextOptions _options;
+
+        public ContextSharedStateTests()
+        {
+            _action = () => _onModelCreatingCallCount += 1;
+            var optionsBuilder = new DbContextOptionsBuilder();
+            optionsBuilder.UseInMemoryDatabase();
+            _options = optionsBuilder.Options;
+
+            _context = new DatabaseContext(_action, _options);
+        }
+
+
+        /// <summary>
+        /// 
+        /// OnModelCreatingShouldBeCalledA and OnModelCreatingShouldBeCalledB
+        /// are identical and both use a DbContext class that is created in the 
+        /// constructor above.
+        /// 
+        /// I expect that each test would run in total isolation and therefore 
+        /// each test would result in the OnModelCreating method running. If the
+        /// tests are run one-at-a-time through the NCrunch Tests window in VS2015
+        /// then this holds true and each test will pass.
+        /// 
+        /// However, often when the tests are run close together, such as when I 
+        /// highlight both tests in the NCrunch Tests window in VS2015 and then
+        /// run the tests, often (but not always), one of the two tests will fail
+        /// stating that the _onModelCreatingCallCount is still 0 which means the
+        /// OnModelCreating method was not called. The only way I believe this can
+        /// happen is if the tests are not totally isolated. Perhaps they are 
+        /// executed using the same process or AppDomain??
+        /// 
+        /// </summary>
+        [Fact()]
+        [Trait("Category", "Instant")]
+        public void OnModelCreatingShouldBeCalledA()
+        {
+            _onModelCreatingCallCount = 0;
+            var entityACount = _context.EntityA.Count();
+            Assert.Equal(1, _onModelCreatingCallCount);
+        }
+        
+        [Fact()]
+        [Trait("Category", "Instant")]
+        public void OnModelCreatingShouldBeCalledB()
+        {
+            _onModelCreatingCallCount = 0;
+            var entityACount = _context.EntityA.Count();
+            Assert.Equal(1, _onModelCreatingCallCount);
+        }
+
+        /// <summary>
+        /// It appears that DbContext's somehow cache the model that
+        /// is created during the OnModelCreating override method so subsequent
+        /// creations of the same DbContext class do not cause the 
+        /// OnModelCreating method to be called. It will only be called 
+        /// the first time. This is shown in the test below.
+        /// </summary>
+        [Fact()]
+        [Trait("Category", "Instant")]
+        public void OnModelCreatingShouldOnlyBeCalledOnce()
+        {
+            _onModelCreatingCallCount = 0;
+
+            _context = new DatabaseContext(_action, _options);
+            var entityACount1 = _context.EntityA.Count();
+            Assert.Equal(1, _onModelCreatingCallCount);
+
+            _context = new DatabaseContext(_action, _options);
+            var entityACount2 = _context.EntityA.Count();
+
+            _context = new DatabaseContext(_action, _options);
+            var entityACount3 = _context.EntityA.Count();
+
+            Assert.Equal(1, _onModelCreatingCallCount);
+        }
+    }
+}
--- Rogero.NcrunchTests.SharedState/DatabaseClasses.cs
+++ Rogero.NcrunchTests.SharedState/DatabaseClasses.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Data.Entity;
 using Microsoft.Data.Entity.Infrastructure;
 
 namespace Rogero.NcrunchTests.SharedState
 {
+    /// <summary>
+    /// 
+    /// A DbContext for interacting with the EntityA type. I do not actually use a 
+    /// real database in these tests. The DbContextOptions parameter to the constructor
+    /// is used to specify that we wil be using an InMemory "database" rather than
+    /// an actual SQL Server-backed database.
+    /// 
+    /// </summary>
     public class DatabaseContext : DbContext
     {
+        /// <summary>
+        /// Action to call when the OnModelCreating method is run.
+        /// </summary>
         private readonly Action _action;
 
         public DatabaseContext(Action action, DbContextOptions options) : base(options)
@@ -26,6 +33,9 @@ namespace Rogero.NcrunchTests.SharedState
         public DbSet<EntityA> EntityA { get; set; }
     }
 
+    /// <summary>
+    /// The simplest entity type possible to use in the DatabaseContext class.
+    /// </summary>
     public class EntityA
     {
         public int Id { get; set; }
