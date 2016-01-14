547bd91560f3a69326e460e756495a6ab1d5b6bd
Paul Rogero <progero@gmail.com>
2016-01-14 08:17:32 -0500

    Initial commit.

---
+++ Rogero.NcrunchTests.SharedState.Tests/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+using Xunit;
+
+namespace Rogero.NcrunchTests.SharedState.Tests
+{
+    public class ContextSharedStateTests
+    {
+        private DatabaseContext _context;
+        private int _onModelCreatingCallCount = 0;
+
+        public ContextSharedStateTests()
+        {
+            Action action = () => _onModelCreatingCallCount += 1;
+            var optionsBuilder = new DbContextOptionsBuilder();
+            optionsBuilder.UseInMemoryDatabase();
+            _context = new DatabaseContext(action, optionsBuilder.Options);
+        }
+
+        [Fact()]
+        [Trait("Category", "Instant")]
+        public void Test1()
+        {
+            var entityACount = _context.EntityA.Count();
+            Assert.Equal(1, _onModelCreatingCallCount);
+        }
+
+        [Fact()]
+        [Trait("Category", "Instant")]
+        public void Test2()
+        {
+            var entityACount = _context.EntityA.Count();
+            Assert.Equal(1, _onModelCreatingCallCount);
+        }
+    }
+}
---
+++ Rogero.NcrunchTests.SharedState/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+
+namespace Rogero.NcrunchTests.SharedState
+{
+    public class DatabaseContext : DbContext
+    {
+        private readonly Action _action;
+
+        public DatabaseContext(Action action, DbContextOptions options) : base(options)
+        {
+            _action = action;
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            _action();
+        }
+
+        public DbSet<EntityA> EntityA { get; set; }
+    }
+
+    public class EntityA
+    {
+        public int Id { get; set; }
+    }
+}
